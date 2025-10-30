5438e3731e241645eee0cec4a15219608a4b2b00
Mykola Turnaviotov <noreply@github.com>
2025-10-30 18:32:38 +0100

    Jenkins fixed for route

--- jenkins.tf
+++ jenkins.tf
@@ -10,11 +10,16 @@ variable "jenkins_admin_password" {
   default     = "admin"
 }
 
+variable "jenkins_name" {
+  default = "jenkins"
+  type    = string
+}
+
 #########################
 # Namespaces
 #########################
 
-# terraform import kubernetes_namespace.dns dns
+# terraform import kubernetes_namespace.jenkins jenkins
 resource "kubernetes_namespace" "jenkins" {
   metadata {
     name = "jenkins"
@@ -22,7 +27,7 @@ resource "kubernetes_namespace" "jenkins" {
 }
 
 #########################
-# Prometheus + Grafana (kube-prometheus-stack)
+# Jenkins Helm Chart
 #########################
 
 # terraform import helm_release.jenkins jenkins/jenkins
@@ -42,7 +47,59 @@ resource "helm_release" "jenkins" {
   ]
 }
 
+# terraform import kubernetes_manifest.k8s_dashboard_ingress 'apiVersion=networking.k8s.io/v1,kind=Ingress,namespace=kubernetes-dashboard,name=dashboard-dns'
+resource "kubernetes_manifest" "jenkins_ingress" {
+  manifest = {
+    apiVersion = "networking.k8s.io/v1"
+    kind       = "Ingress"
+    metadata = {
+      name      = "ingress-${var.jenkins_name}"
+      namespace = kubernetes_namespace.jenkins.metadata[0].name
+      annotations = {
+        "kubernetes.io/ingress.class"                      = "traefik"
+        "cert-manager.io/cluster-issuer"                   = "local-ca"
+        "external-dns.alpha.kubernetes.io/hostname"        = "${var.jenkins_name}.${var.dns_private_zone_name}"
+        "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+      }
+    }
+
+    spec = {
+      ingressClassName = "traefik"
+      rules = [
+        {
+          host = "${var.jenkins_name}.${var.dns_private_zone_name}"
+          http = {
+            paths = [
+              {
+                path     = "/"
+                pathType = "Prefix"
+                backend = {
+                  service = {
+                    name = var.jenkins_name
+                    port = {
+                      number = 8080
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+
+      tls = [
+        {
+          hosts      = ["${var.jenkins_name}.${var.dns_private_zone_name}"]
+          secretName = "${var.jenkins_name}-tls"
+        }
+      ]
+    }
+  }
+
+  depends_on = [kubernetes_namespace.jenkins]
+}
+
 output "jenkins_admin_password" {
-  value = "password is:\nk -n ${kubernetes_namespace.jenkins.metadata[0].name} get secrets jenkins -o json | jq '.data | map_values(@base64d)'"
+  value = "password is:\nkubectl -n ${kubernetes_namespace.jenkins.metadata[0].name} get secrets jenkins -o json | jq '.data | map_values(@base64d)'"
 }
 
