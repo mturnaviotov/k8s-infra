b439e0b91740393ea9954392f1334d564938fe25
Mykola Turnaviotov <noreply@github.com>
2025-11-06 17:43:39 +0100

    ArgoCD added, readme updated

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Intro
 
-Here i will collect all Kubernetes related basic Ci/CD stuff, like
-Networks, DNS, monitoring, CI/CD
+Here i will collect all Kubernetes related basic CI/CD stuff, like
+Networks, DNS, monitoring, CI/CD for providing whole Infrastructure as a code
 
 # Components
 
@@ -12,3 +12,15 @@ Networks, DNS, monitoring, CI/CD
 - [x] K8S Dashboard (UI)
 - [x] KeyCloak      (AAA)
 - [x] Jenkins       (CI/CD)
+- [x] ArgoCD        (CD)
+- [x] Test ToDo Application deployed via ArgoCD (CD)
+
+# TODO:
+
+1. Link all services to KeyCloak as main user database
+2. Add Nexus as Docker image and helm registry
+
+## Notices
+
+1. You should build all images locally for you before use - PowerDNS, ToDo, etc.
+2. ArgoCD Cluster will be in UNKNOWN state until first deploy
---
+++ argocd.tf
@@ -0,0 +1,198 @@
+######################### # Variables #########################
+variable "argocd_cluster_url" {
+  description = "ArgoCD cluster URL"
+  type        = string
+  default     = "https://host.docker.internal:26443"
+}
+
+#########################
+# Namespace
+#########################
+resource "kubernetes_namespace" "argocd" {
+  metadata { name = "argocd" }
+}
+
+#########################
+# ArgoCD (Helm)
+#########################
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = kubernetes_namespace.argocd.metadata[0].name
+  create_namespace = false
+
+  values = [<<EOF
+global:
+  domain: argocd.zone.internal
+server:
+  ingress:
+    enabled: true
+controller:
+  replicaCount: 1
+redis:
+  enabled: true
+EOF
+  ]
+
+}
+
+# add to config map argocd-cm for custom accounts and add admin user token login
+# kubectl -n argocd edit configmap argocd-cm
+# example:
+# data:
+#   accounts.admin: apiKey, login
+#   accounts.tech: apiKey, login
+#   accounts.tech.enabled: 'true'
+
+#########################
+# Service Account + RoleBinding
+#########################
+
+resource "kubernetes_service_account" "argocd_access" {
+  metadata {
+    name      = "argocd-access"
+    namespace = kubernetes_namespace.argocd.metadata[0].name
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "argocd_access_binding" {
+  metadata {
+    name = "argocd-access-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.argocd_access.metadata[0].name
+    namespace = kubernetes_service_account.argocd_access.metadata[0].namespace
+  }
+}
+
+# Token (1.24+)
+resource "kubernetes_secret_v1" "argocd_access_token" {
+  metadata {
+    name      = "argocd-access-token"
+    namespace = kubernetes_service_account.argocd_access.metadata[0].namespace
+    annotations = {
+      "kubernetes.io/service-account.name" = kubernetes_service_account.argocd_access.metadata[0].name
+    }
+  }
+  type = "kubernetes.io/service-account-token"
+}
+
+#########################
+# ConfigMap (CA Cert)
+#########################
+
+data "kubernetes_config_map" "kube_root_ca" {
+  metadata {
+    name      = "kube-root-ca.crt"
+    namespace = kubernetes_namespace.argocd.metadata[0].name
+  }
+}
+
+#########################
+# ArgoCD Cluster Secret
+#########################
+
+resource "kubernetes_secret" "argocd_cluster_orbstack" {
+  metadata {
+    name      = "orbstack"
+    namespace = kubernetes_namespace.argocd.metadata[0].name
+    labels = {
+      "argocd.argoproj.io/secret-type" = "cluster"
+    }
+  }
+
+  data = {
+    name   = ("orbstack")
+    server = var.argocd_cluster_url
+    config = jsonencode({
+      bearerToken = kubernetes_secret_v1.argocd_access_token.data["token"]
+      tlsClientConfig = {
+        insecure = true
+        # Uncomment to use CA cert, also you need to disable insecure in real world usage
+        #caData   = base64encode(data.kubernetes_config_map.kube_root_ca.data["ca.crt"])
+      }
+    })
+  }
+}
+
+#########################
+# Outputs
+#########################
+
+output "argocd_initial_admin_password" {
+  description = "Initial ArgoCD admin password (decoded)\n"
+  value       = "\n=====\nArgoCD admin password:\nkubectl -n ${kubernetes_namespace.argocd.metadata[0].name} get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 --decode\n"
+}
+
+##########################
+# ArgoCD Application + Project
+##########################
+
+# terraform import kubernetes_manifest.argocd_project_todo 'apiVersion=argoproj.io/v1alpha1,kind=AppProject,namespace=argocd,name=todo'
+resource "kubernetes_manifest" "argocd_project_todo" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "AppProject"
+    metadata = {
+      name      = "todo"
+      namespace = kubernetes_namespace.argocd.metadata[0].name
+    }
+    spec = {
+      description = "Todo application project for testing"
+      sourceRepos = ["https://github.com/mturnaviotov/go-k8s-test-app.git"]
+      destinations = [{
+        namespace = "todo"
+        server    = var.argocd_cluster_url
+      }]
+      clusterResourceWhitelist = [{
+        group = "*"
+        kind  = "*"
+      }]
+      namespaceResourceWhitelist = [{
+        group = "*"
+        kind  = "*"
+      }]
+    }
+  }
+}
+
+# terraform import kubernetes_manifest.argocd_app_todo 'apiVersion=argoproj.io/v1alpha1,kind=Application,namespace=argocd,name=todo'
+resource "kubernetes_manifest" "argocd_app_todo" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "todo"
+      namespace = kubernetes_namespace.argocd.metadata[0].name
+    }
+    spec = {
+      project = "todo"
+      source = {
+        repoURL        = "https://github.com/mturnaviotov/go-k8s-test-app.git"
+        targetRevision = "HEAD"
+        path           = "charts"
+        helm = {
+          valueFiles = ["values.yaml"]
+        }
+      }
+      destination = {
+        server    = var.argocd_cluster_url
+        namespace = "todo"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = ["CreateNamespace=true"]
+      }
+    }
+  }
+}
