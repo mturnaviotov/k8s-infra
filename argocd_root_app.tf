9c049b88b78272e4fc6c55e0acb45716fab12f25
Mykola Turnaviotov <noreply@github.com>
2025-11-24 12:41:44 +0100

    ArgoCD root app changed

---
+++ argocd_root_app.tf
@@ -0,0 +1,34 @@
+##########################
+# ArgoCD Root Application
+##########################
+
+# terraform import kubernetes_manifest.argocd_root_app 'apiVersion=argoproj.io/v1alpha1,kind=Application,namespace=argocd,name=root'
+resource "kubernetes_manifest" "argocd_root_app" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "root"
+      namespace = kubernetes_namespace.ns["argocd"].metadata[0].name
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/mturnaviotov/k8s-argo.git"
+        targetRevision = "HEAD"
+        path           = "root/"
+      }
+      destination = {
+        server    = var.argocd_cluster_url
+        namespace = kubernetes_namespace.ns["argocd"].metadata[0].name
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = ["CreateNamespace=true"]
+      }
+    }
+  }
+}
