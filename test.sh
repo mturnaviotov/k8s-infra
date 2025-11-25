3ed58dcc4a42df71a0d45e96cec6dae2dd74d549
Mykola Turnaviotov <noreply@github.com>
2025-11-25 18:06:58 +0100

    kube prom stack default file global clearance

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+declare -a appEnv=("todo" "todo-dev" "todo-prod")
+    
+for appEnv in "${appEnv[@]}"; do
+    echo $appEnv
+    count=0
+    while [ $count -lt 10 ]; do
+        let "count=count+1"
+        curl -s -X POST "http://${appEnv}-backend.${appEnv}.svc.cluster.local:8080/todos" \
+            -H 'accept: application/json' -H 'Content-Type: application/json' \
+            -d '{"text":"It ${count} todo" }'
+        done
+    curl "http://${appEnv}-backend.${appEnv}.svc.cluster.local:8080/metrics"
+done
+exit 0
+
+    curl -s -X POST 'http://todo-backend.todo.svc.cluster.local:8080/todos' \
+      -H 'accept: application/json' -H 'Content-Type: application/json' \
+      -d '{"text":"First test todo" }'
