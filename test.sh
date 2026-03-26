fa943d8f0336ae2c05b8000fdd8115f41ecb3acc
Mykola Turnaviotov <noreply@github.com>
2026-03-26 23:37:02 +0100

    test script changed

--- test.sh
+++ test.sh
@@ -6,26 +6,9 @@ for appEnv in "${appEnv[@]}"; do
     count=0
     while [ $count -lt 10 ]; do
         let "count=count+1"
-        curl -s -X POST "http://${appEnv}2-backend.${appEnv}.svc.cluster.local:8080/todos" \
+        curl -s -X POST "http://${appEnv}-backend.${appEnv}.svc.cluster.local:8080/todos" \
             -H 'accept: application/json' -H 'Content-Type: application/json' \
             -d "{\"text\":\"It ${count} todo\" }"
         done
     curl "http://${appEnv}-backend.${appEnv}.svc.cluster.local:8080/metrics"
 done
-exit 0
-
-# #!/bin/bash
-# declare -a appEnv=("todo")
-# #todo2-backend.todo.svc.cluster.local:8080
-# for appEnv in "${appEnv[@]}"; do
-#     echo "=== $appEnv ===="
-#     count=0
-#     while [ $count -lt 10 ]; do
-#         let "count=count+1"
-#         curl -s -X POST "http://${appEnv}2-backend.${appEnv}.svc.cluster.local:8080/todos" \
-#             -H 'accept: application/json' -H 'Content-Type: application/json' \
-#             -d "{\"text\":\"It ${count} todo\" }"
-#         done
-#     curl "http://${appEnv}-backend.${appEnv}.svc.cluster.local:8080/metrics"
-# done
-# exit 0
